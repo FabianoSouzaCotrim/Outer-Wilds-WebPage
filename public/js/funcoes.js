7a8a73069f76b97eae8474ab84560b7db318a212
fabiano souza <fabianokurashi@gmail.com>
2022-05-30 13:54:49 -0300

    implementação api login

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -5,11 +5,11 @@ function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
+    var nomeUsuario = document.getElementById("nomeUsuario");
 
     if (email != null && nome != null) {
         // window.alert(`Seja bem-vindo, ${nome}!`);
-        b_usuario.innerHTML = nome;
+        nomeUsuario.innerHTML = nome;
 
         // finalizarAguardar();
     } else {
--- src/database/config.js
+++ src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "outerWilds",
-    user: "root",
-    password: "4242",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
